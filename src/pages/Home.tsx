89fb1043b4713129e1007b04cc0fa5992fc0661e
GatereKinyanjui <gaterekinyanjui@gmail.com>
2023-04-08 11:37:16 +0300

    adds all new files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SignIn } from "./SignIn";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <h1 className="homePageTitle">Home</h1>
+      <SignIn />
+    </div>
+  );
+};
